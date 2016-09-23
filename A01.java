4400c163e62bdce364381fd6b3e78abe360d7222
dmdoa12 <dmdoa12@gmail.com>
2016-09-23 16:04:58 +0900

    A01 완성

--- A01.java
+++ A01.java
@@ -1,2 +1,26 @@
+import java.util.*;
 class A01{
+	public static void main(String[] args){
+		new A01().start();
+	}
+	
+	void start(){
+		Scanner s = new Scanner(System.in);
+		System.out.print("태어난 년도를 입력하세요 :");
+
+		int year = s.nextInt();
+		
+		int age = 2014-year+1;
+
+		if(age < 20 && age > 0){
+			print("미성년자 입니다.");
+		}
+		else{
+			print("미성년자가 아닙니다.");
+		}
+	}
+
+	void print(String str){
+		System.out.print(str);
+	}
 }
